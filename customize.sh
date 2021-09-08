924cc8a2b2b021370eaa9365d2109c2227e13954
Luca Carta <lcarta.mail@gmail.com>
2021-09-08 17:49:37 +0200

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 #
 
-cp -R luci-static/argon /www/luci-static/.
+cd /tmp
 
-cp -R themes/argon /usr/lib/lua/luci/view/themes/.
+wget -O repo.tar.gz https://api.github.com/repos/microsard81/speed-up/tarball ; tar x -zv -f repo.tar.gz ; rm -f repo.tar.gz ; cd microsard81-speed-up*
 
-rm -fR luci-static ; rm -fR themes
+#cp -R luci-static/argon /www/luci-static/.
+
+#cp -R themes/argon /usr/lib/lua/luci/view/themes/.
+
+#rm -fR luci-static ; rm -fR themes
