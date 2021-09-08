1f90408ebcba224a859bd2223634ba9133e2a269
Luca Carta <lcarta.mail@gmail.com>
2021-09-08 18:08:26 +0200

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,18 +2,14 @@
 #
 
 current=`pwd`
-
 cd /tmp
-
-wget -O repo.tar.gz https://api.github.com/repos/microsard81/speed-up/tarball ; tar x -zv -f repo.tar.gz ; rm -f repo.tar.gz ; cd microsard81-speed-up*
-
+wget -O repo.tar.gz https://api.github.com/repos/microsard81/speed-up/tarball
+tar x -zv -f repo.tar.gz
+rm -f repo.tar.gz 
+cd microsard81-speed-up*
 cp -R luci-static/argon /www/luci-static/.
 cp -R themes/argon /usr/lib/lua/luci/view/themes/.
-
 cd /tmp
-
 rm -fR microsard81-speed-up*
-
 cd $current
-
 rm -- "$0"
